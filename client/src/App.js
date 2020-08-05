d6944279c0006547ed13fb1d19e8d14f9a28cdf0
Jonathan Kelley <jonathankelley87@gmail.com>
2020-08-05 15:40:35 -0400

    addition of static.json file

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,39 @@
-import React from "react";
+import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/index";
 import Jumbotron from "./components/Jumbotron/index";
 import Saved from "./containers/saved";
 import Search from "./containers/search";
 
-function App() {
-  return (
-    <Router>
+class App extends Component {
+  static propTypes = {
+    children: PropTypes.node
+  }
+  
+  render() {
+    const { children } = this.props
+    return (
+      <Router>
         <Navbar />
         <Jumbotron />
         <Route exact path="/" component={Search} />
         <Route exact path="/search" component={Search} />
         <Route exact path="/saved" component={Saved} />
     </Router>
-  );
-}
+    )
+  }
+  }
+
+// function App() {
+//   return (
+//     <Router>
+//         <Navbar />
+//         <Jumbotron />
+//         <Route exact path="/" component={Search} />
+//         <Route exact path="/search" component={Search} />
+//         <Route exact path="/saved" component={Saved} />
+//     </Router>
+//   );
+// }
 
 export default App;
