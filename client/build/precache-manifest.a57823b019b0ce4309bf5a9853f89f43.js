7f13ab56119b33a2ba52bc1bfa6fa996a7c0db15
Jonathan Kelley <jonathankelley87@gmail.com>
2020-08-04 23:07:42 -0400

    install of npm again

---
+++ client/build/precache-manifest.a57823b019b0ce4309bf5a9853f89f43.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "b2a158b5edc48ed7736e",
+    "url": "/googlereactsearch/static/js/main.d501f46a.chunk.js"
+  },
+  {
+    "revision": "c625b54d2a62fa7364b4",
+    "url": "/googlereactsearch/static/js/runtime~main.71f7300b.js"
+  },
+  {
+    "revision": "ab1c633f9834b115a3eb",
+    "url": "/googlereactsearch/static/js/2.bbe8eb11.chunk.js"
+  },
+  {
+    "revision": "5b7c677a8ad361388070d6e86b06fe22",
+    "url": "/googlereactsearch/index.html"
+  }
+];
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d5734a48aad3a4b5aefac9f1c259dfb9.js"
+  "/googlereactsearch/precache-manifest.a57823b019b0ce4309bf5a9853f89f43.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/googlereactsearch/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
